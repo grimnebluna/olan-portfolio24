8d518576d53a26f1b9a92641bdec794c007298a8
Olivier Zurkirchen <oz@x02.ch>
2024-05-03 21:06:52 +0200

    x

--- src/pages/vita.js
+++ src/pages/vita.js
@@ -13,7 +13,7 @@ const vitaPage = ({ data }) => (
       <div className="oz-wrapper-content">
         
         <div className="contentdiv">
-          <h2 className={styles.h2heading}>About</h2>
+          <h2 className={styles.h2heading}>About Olan!</h2>
 
            {/* instead of the nl trick, use dangerouslySetInnerHTML */}
             <div dangerouslySetInnerHTML={{ __html: data.wpPage.aboutcontact.about }} />
