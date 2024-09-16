865ab139dc8af53489ddc28c00ddf753cea86ec2
Olivier Zurkirchen <oz@x02.ch>
2024-09-16 18:15:35 +0200

    x

--- src/pages/bubbbbbles.js
+++ src/pages/bubbbbbles.js
@@ -15,17 +15,17 @@ const bubblesPage2 = ({ data }) => (
         <div className="contentdiv">
           <h2 className={styles.h2heading}>Bubbbbbles #2</h2>
           <p>Sat. 26th October 2024, <a href="https://cafete.ch/" target="_blank" rel="noopener">Cafete Reitschule Bern</a></p>
-          <p><a href="https://soundcloud.com/olangalactica" target="_blank" rel="noopener">Olan!</a> [0x01- ZH]</p>
-          <p>Bernet Branca [130bpm]</p>
-          <p>DJ TBA</p>
-          <p>Electro / House / Techno / Rave</p>
+          <p><a href="https://soundcloud.com/olangalactica" target="_blank" rel="noopener">Olan!</a> [0x01 - ZH]</p>
+          <p><a href="https://soundcloud.com/caracvierzmr" target="_blank" rel="noopener">Carac</a> [vierzmr - ZH]</p>
+          <p>Mene [ZH]</p>
+          <p>Electro / Techno / Rave / Dubstep / Hyperpop / Donk / Breaks</p>
           <p>With the Bubbbbbles Party, Olan! brings their favorite DJs from their bubble to Bern. High-quality baller music is to be expected.</p>
 
 
       </div>
       <div className="contentdiv">
       <StaticImage
-          src="../images/bubbbbles2.webp"
+          src="../images/bubblesbild2-small.webp"
           loading="eager"
           quality={95}
           formats={["auto", "webp", "avif"]}
