a11936f5a0bb08b808d12a9c4b1d8cae37cfc0d4
Olivier Zurkirchen <oz@x02.ch>
2024-12-29 07:40:10 +0100

    x

--- src/pages/spikes-and-bubbles.js
+++ src/pages/spikes-and-bubbles.js
@@ -16,6 +16,7 @@ const bubblesPage2 = ({ data }) => (
           <h2 className={styles.h2heading}>Spikes & Bubbles</h2>
           <p>release date: 30 dec 2024 on <a href="https://deedeespicks.bandcamp.com/" target="_blank" rel="noopener">Dee Dee's picks</a></p>
           <a href="https://deedeespicks.bandcamp.com/album/ddp016-spikes-bubbles" target="_blank" rel="noopener" class="buybtn button">Bandcamp</a>
+          <a href="https://soundcloud.com/olangalactica/sets/spikes-bubbles" target="_blank" rel="noopener" class="buybtn button">Soundcloud</a>
           <a href="https://www.youtube.com/watch?v=GxiaOtEY-gA&list=OLAK5uy_nb0dxKoMifEyWmUuDGmqtJy-8nP_K8d_8" target="_blank" rel="noopener" class="buybtn button">Youtube</a>
           <a href="https://music.apple.com/ch/album/spikes-bubbles/1782295722" target="_blank" rel="noopener" class="buybtn button">Apple Music</a>
           <a href="https://open.spotify.com/album/0W7SEu7fBslr5WRLCqKORg?si=SfkEm9HeQhi0hzrXzvr7rA" target="_blank" rel="noopener" class="buybtn button">Spotify</a>
